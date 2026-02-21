a3fb93dadb79283ce6bd9317c2d7b04ad11bda03
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-22 03:19:58 +0900

    Delete public directory

--- public/assets/js/frame.js
+++
@@ -1,6 +0,0 @@
-const home = () => {
-  const iframe = document.getElementById('proxy-iframe');
-  iframe.src = '/proxy.html';
-};
-
-document.getElementById('home-btn').addEventListener('click', home);
